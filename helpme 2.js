91bd94187447fe019cd0f9567127bec48bf70f8f
juandiospvpnime <juandiospvp@gmail.com>
2025-08-02 13:46:30 -0400

    a
    a

---
+++ helpme 2.js
@@ -0,0 +1,75 @@
+let edad=prompt("ingrese su edad:");
+edad =+edad
+if(edad >= 18) {
+    mensaje = "mayor"};
+    else if (edad <18){
+    edad = "menor de edad";
+    mensaje = "menor";
+    }
+console.log(`eres ${mensaje} de edad`)
+
+
+
+
+
+
+
+
+let num1=prompt("ingrese numero 1:");
+let num2=prompt("ingrese numero 2:");
+a =+ num1
+b =+ num2
+if (num1==um2) {mensaje = "son iguales" }
+    else if (num1>num2) {mensaje = num1}
+    else {mensaje = ""}
+console.log(` ${mensaje}`)
+
+
+let colorSemaforo=prompt("ingrese color del semaforo:");
+rojo=""
+amarillo=""
+verde=""
+if (colorSemaforo=rojo){mensaje = " no cruse"};
+    else if (colorSemaforo=amarillo){mensaje = "precausion"};
+    else (mensaje= "puede pasar")
+console.log(`${mensaje}`);
+
+
+
+
+let semana= Number (prompt("que diga de la semana es? (escribe un numero));
+console.log (`has selecionado ${semana} que es el dia`
+switch(semana)
+    case: 1
+     console.log("es el dia lunes");
+        break;
+    case: 2
+    console.log("es el dia martes");
+        break;
+    case: 3
+    console.log("es el dia miercoles");
+      break;
+    case: 4
+     console.log("es el dia jueves");
+        break;
+     case: 5
+    console.log("es el dia viernes");
+          break;
+    case: 6
+    console.log("es el dia sabado");
+     break;
+     case: 7
+     console.log("es el dia domingo")
+     break
+default
+console.log ("escribe un numero valido");
+
+
+
+
+
+
+
+
+
+
---
+++ helpme.js
@@ -0,0 +1,49 @@
+let nombre= "juan"
+let edad = "14"
+let esTUDIANTE = "si"
+console.log(`
+${nombre}
+${edad}
+${esTUDIANTE }
+`)
+const variable = {
+    nombre_usuario: nombre,
+    edad_usuario: edad,
+    esTUDIANTE_usuario: esTUDIANTE
+}
+
+
+
+
+
+
+
+let nomb=prompt("ingrese su nombre");
+let añonac=prompt("ingrese su año de nacimiento");
+const ap=2025;
+let restas = ap-añonac
+console.log(`Edad del usuario ${restas}`)
+
+
+
+
+let a=prompt("ingrese numero 1:")
+let b=prompt("ingrese numero 2:")
+a = +a;
+b = +b
+let suma = a + b; 
+let resta = a - b; 
+let multiplicacion = a * b; 
+let division = a / b; 
+let modulo = a % b; 
+let exponenciacion = a ** b;
+console.log(`Suma: ${suma}`);   
+console.log(`Resta: ${resta}`);
+console.log(`Multiplicación: ${multiplicacion}`);
+console.log(`División: ${division}`);
+console.log(`Módulo: ${modulo}`);   
+console.log(`Exponenciación: ${exponenciacion}`);
+
+
+
+
