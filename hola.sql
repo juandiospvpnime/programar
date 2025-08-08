25a0657fc349decba92f1af471472bbc32c1788a
juandiospvpnime <juandiospvp@gmail.com>
2025-08-08 14:07:08 -0400

    Add files via upload

---
+++ hola.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Estudiante (
+    Estudiante (14) PRIMARY KEY
+    nombre VARCHAR(100),
+    apellido VARCHAR(100),
+    cedula INT,
+INSERT INTO Estudiante (nombre,apellido,,cedula) VALUES
+    (1, 'roberto', 'jimenez','16186354',),
+    (2, 'jinmy', 'chacin','29767741',),
+    (3, 'maria', 'urdaneta','30604870',),
+    (4, 'daniel', 'sandoval','30820000',),
+    (5, 'a', 'sandoval','30820000',),
+
+
+
+
+);
+
+
+
+
+
+
+CREATE TABLE Estudiante (
+    customer_id INT PRIMARY KEY,
+    inv_nombre VARCHAR(100),
+    inv_apellido VARCHAR(100),
+    age INT,
+    cedula INT,
+    country VARCHAR(100)
+);
