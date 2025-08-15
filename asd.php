09802e0fb90d50fae910d1cf3af4a4ed1c504dd4
juandiospvpnime <juandiospvp@gmail.com>
2025-08-15 13:53:19 -0400

    Add files via upload

---
+++ asd.php
@@ -0,0 +1,69 @@
+<?php
+
+
+$nombre = "juan";
+$edad = "14";
+$descripcion = "hola soy juan diego arrieta tengo 14 años y soy un humano";
+
+echo "Mis datos son 
+Mi nombre es $nombre
+Mi edad es $edad
+Mi descripcion es $descripcion
+";
+
+
+
+$num1 = 40;
+$num2 = 45;
+$suma = $num1 + $num2;
+echo " el sultado de la suma " . $suma . "";
+echo " el resultado es " . ($num1 + $num2);
+
+
+<?php
+echo "numero pares
+" ;
+for ($i = 2 ; $i <= 50 ; $i+=2){
+  echo $i . " ";
+
+}
+
+echo "
+numero impares
+";
+for ($i = 1 ; $i <= 50 ; $i+=2){
+    echo $i . " ";
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    echo "8 x " . $i . " = " . (8 * $i) . "\n";
+}
+echo "\n";
+?>
+
+echo "
+numero impares
+";
+for ($i = 1 ; $i <= 50 ; $i+=2){
+    echo $i . " ";
+    
+}
+
+
+
+
+
